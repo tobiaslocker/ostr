e39703dcf2430227a9b1c6b783f22ef3ea9fbdb2
tobiaslocker <tobias@tobiaslocker.de>
2019-08-18 14:17:26 +0100

    Change to east-const

--- include/ostr.hpp
+++ include/ostr.hpp
@@ -5,35 +5,41 @@
 
 template <typename T>
 class manipulator {
-  std::function<std::ostream&(std::ostream&, T)> m_func;
+  std::function<std::ostream &(std::ostream &, T)> m_func;
   T m_arg;
 
  public:
-  manipulator(std::function<std::ostream&(std::ostream&, T)> f, T arg)
+  manipulator(std::function<std::ostream &(std::ostream &, T)> f, T arg)
       : m_func(f), m_arg(arg) {}
-  void do_op(std::ostream& str) const { m_func(str, m_arg); }
+  void do_op(std::ostream &str) const { m_func(str, m_arg); }
 };
 
 template <typename T>
-class do_print : public manipulator<const std::vector<T>&> {
-  static std::ostream& call(std::ostream& os, const std::vector<T>& v) {
+class do_print : public manipulator<std::vector<T> const &> {
+  static std::ostream &call(std::ostream &os, std::vector<T> const &v) {
     os << "{ ";
     std::copy(v.begin(), v.end(), std::ostream_iterator<T>(std::cout, ", "));
     return os << "}";
   }
+
  public:
-  do_print(const std::vector<T>& v)
-      : manipulator<const std::vector<T>&>(call, v) {}
+  do_print(std::vector<T> const &v)
+      : manipulator<std::vector<T> const &>(call, v) {}
 };
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const manipulator<T>& m) {
+std::ostream &operator<<(std::ostream &os, manipulator<T> const &m) {
   if (!os.good()) return os;
   m.do_op(os);
   return os;
 }
 
 template <class T>
-do_print<T> print(const std::vector<T>& v) {
+do_print<T> print(std::vector<T> const &v) {
+  return do_print<T>(v);
+}
+
+template <class T>
+do_print<T> print(T const &v) {
   return do_print<T>(v);
 }
