e96241b6e0c844ff9872bacd49df43d64d21e79b
tobiaslocker <tobias@tobiaslocker.de>
2019-09-03 15:09:31 +0100

    Add vector<int> support

--- include/ostr.hpp
+++ include/ostr.hpp
@@ -1,11 +1,17 @@
-#include <any>
+﻿#include <any>
 #include <functional>
 #include <iomanip>
 #include <iostream>
 #include <vector>
 
 std::ostream &format_any(std::ostream &os, std::any const &value) {
-  if (auto s = std::any_cast<int>(&value)) {
+  if (auto s = std::any_cast<bool>(&value)) {
+    os << (*s ? "true" : "false");
+  } else if (auto s = std::any_cast<int>(&value)) {
+    os << *s;
+  } else if (auto s = std::any_cast<uint8_t>(&value)) {
+    os << (*s & 0xf);
+  } else if (auto s = std::any_cast<unsigned short>(&value)) {
     os << *s;
   } else if (auto s = std::any_cast<double>(&value)) {
     os << *s;
@@ -15,6 +21,17 @@ std::ostream &format_any(std::ostream &os, std::any const &value) {
     os << std::quoted(*s);
   } else if (auto s = std::any_cast<char>(&value)) {
     os << '\'' << *s << '\'';
+  } else if (auto v = std::any_cast<std::vector<int>>(&value)) {
+    os << "[";
+    if (!v->empty()) {
+      std::for_each(v->begin(), v->end() - 1, [&](auto const &arg) {
+        format_any(os, arg) << ", ";
+      });
+      format_any(os, v->back());
+    }
+    os << "]";
+  } else {
+    os << value.type().name() << "Moep";
   }
   return os;
 }
